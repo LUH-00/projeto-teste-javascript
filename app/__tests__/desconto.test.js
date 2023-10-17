2870abf019e05904cd2ede69bad25fa38e54f55d
bruno oliveira <brunooliveira.2115b@gmail.com>
2023-10-16 21:02:55 -0300

    iniciado teste em javascript

---
+++ app/__tests__/desconto.test.js
@@ -0,0 +1,9 @@
+const freteGratis = require("../desconto")
+
+test("frete será verdadeiro para valores maior ou igual a 150", ()=>{
+    expect(freteGratis(200)).toBeTruthy()// Espera que o resultado seja verdadeiro
+})
+
+test("Frete será verdadeiro para valores menores ou igual a zero", ()=>{
+    expect(freteGratis(-8)).toBeLessThanOrEqual(0)
+})
---
+++ app/desconto.js
@@ -0,0 +1,10 @@
+function freteGratis(valor){
+    if (valor > 0 ) {
+        return valor >=150 
+    } else {
+        return valor
+    }
+    
+}
+
+module.exports = freteGratis // Exportando a funcção com um módulo do node.js
